c7f198f58b30766df5218901c5b5c3e26c3828f7
Jesus Torrejon <jtorrejonl@uni.pe>
2021-07-25 04:01:23 -0500

    entrenamiento y carga de modelos

--- tests/transformer/Modules.py
+++ tests/transformer/Modules.py
@@ -1,38 +1,35 @@
 import torch
-from torch.functional import Tensor
 import torch.nn as nn
 import numpy as np
-from typing import Tuple
 
 __author__ = "Yu-Hsiang Huang"
 
 
 class ScaledDotProductAttention(nn.Module):
-    """
-    La entrada consiste de vectores consultas Q, K, V y vectores claves de dimensión d_k, y valores de dimensión d_v. Luego se sigue una serie de operaciones, resultando la salida como una suma ponderada de los valores.
-    """
+    """ Scaled Dot-Product Attention """
 
-    def __init__(self, temperature: float, attn_dropout: float = 0.0):
+    def __init__(self, temperature, attn_dropout=0.0):
         super().__init__()
         self.temperature = temperature
-        # el dropout es la probabilidad de eliminar las conexiones en la FF para evitar overfitting
-        self.dropout = nn.Dropout(p=attn_dropout) if attn_dropout != 0 else None
+        self.dropout = nn.Dropout(attn_dropout) if attn_dropout != 0 else None
         self.softmax = nn.Softmax(dim=2)
 
-    def forward(self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor = None) -> Tuple[Tensor]:
-        # bmm realiza un producto MatMul(Q, K)
-        attn: Tensor = torch.bmm(input=q, mat2=k.transpose(dim0=1, dim1=2))
-        # Scale
-        attn: Tensor = attn / self.temperature
+    def forward(self, q, k, v, mask=None):
+        print ("*"*30)
+        print (type(q))
+        print (type(k))
+        print (q.size())
+        print (k.size())
+        print ("*"*30)
+        attn = torch.bmm(q, k.transpose(1, 2))
+        attn = attn / self.temperature
 
-        # Mask
         if mask is not None:
-            attn = torch.masked_fill(mask, -np.inf)
-        # SoftMax
-        attn: Tensor = self.softmax(attn)
+            attn = attn.masked_fill(mask, -np.inf)
+
+        attn = self.softmax(attn)
         if self.dropout is not None:
             attn = self.dropout(attn)
-
-        # MatMul
         output = torch.bmm(attn, v)
+
         return output, attn
